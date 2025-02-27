318c2fb4e0bd443f5671106ab7dad44f0f4b9ac4
zanielsen162 <zoeynielse162@gmail.com>
2025-02-27 12:34:51 -0800

    testing

--- ilp.py
+++ ilp.py
@@ -3,6 +3,7 @@ from gurobipy import GRB
 import numpy as np
 import pandas as pd
 from classes import *
+from ilp_testing import *
 
 # Construct the model
 # Input: 
@@ -11,22 +12,6 @@ from classes import *
 #   list of edges between courses and applicants
 # Output:
 #   solution of the optimization problem
-
-course_a = Course('a', [], 2, []) 
-course_b = Course('b', [], 1, [])
-
-ta_1 = Applicant('1', 4.0, True, [], [], [], [])
-ta_2 = Applicant('2', 3.0, True, [], [], [], [])
-ta_3 = Applicant('3', 2.0, False, [], [], [], []) 
-
-courses = [course_a, course_b]
-tas = [ta_1, ta_2, ta_3]
-edges = [Edge(ta, course) for course in courses for ta in tas]
-
-rankings = {
-    course_a: [(Edge(ta_1, course_a), 2), (Edge(ta_2, course_a), 1), (Edge(ta_3, course_a), 2)],
-    course_b: [(Edge(ta_1, course_b), 1), (Edge(ta_2, course_b), 0), (Edge(ta_3, course_b), 1)]
-} # dictionary where keys are courses and the values are lists of tuples (edge, ranking)
 
 # have rankings be a list of tuples (TA, ranking)
 def evaluation_function(course, rankings, vars):
@@ -38,9 +23,9 @@ def evaluation_function(course, rankings, vars):
     value = (1 / denominator) * sum([vars[entry[0]] * (entry[1] - B) for entry in course_rankings])
     return value
 
-
+# for my own testing
 def eval_general(course, rankings, tas):
-    sorted_val = sorted(list(rankings.values()))
+    sorted_val = sorted(rankings[course],key=lambda x: x[1], reverse=True)
     B = sorted_val[course.ta_req_nbr-1]
 
     denominator = (course.ta_req_nbr * max([np.abs(rankings[ta] - B) for ta in tas])) if (max([np.abs(rankings[ta] - B) for ta in tas])) != 0 else course.ta_req_nbr
@@ -48,34 +33,35 @@ def eval_general(course, rankings, tas):
 
     return value
 
-print(eval_general(course_a, {ta_2: 0, ta_3: 1}, [ta_2, ta_3]))
-
+# function that given the courses, rankings, and available matching constructs the ILP
 def build_model(courses, rankings, edges):
     model = gp.Model("course_allocation")
-    # model.setParam('OutputFlag', 0)
+    model.setParam('OutputFlag', 0)
 
     edge_vars = {}
     for edge in edges:
         edge_vars[edge] = model.addVar(vtype=GRB.BINARY, name=f'assign_{edge.course.id}_{edge.ta.id}')
 
-    m = model.addVar(vtype=GRB.CONTINUOUS, name='m')
-    M = model.addVar(vtype=GRB.CONTINUOUS, name='M')
+    m = model.addVar(vtype=GRB.CONTINUOUS, lb=-GRB.INFINITY, name='m')
+    M = model.addVar(vtype=GRB.CONTINUOUS, lb=-GRB.INFINITY, name='M')
         
     model.addConstrs(gp.quicksum(edge_vars[edge] for edge in edges if edge.ta == ta) == 1 for ta in tas if ta.class_level)
     model.addConstrs(gp.quicksum(edge_vars[edge] for edge in edges if edge.ta == ta) <= 1 for ta in tas if not ta.class_level)
     model.addConstrs(gp.quicksum(edge_vars[edge] for edge in edges if edge.course == course) == course.ta_req_nbr for course in courses)
     model.addConstrs(m <= evaluation_function(course, rankings, edge_vars) for course in courses)
     model.addConstrs(M >= evaluation_function(course, rankings, edge_vars) for course in courses)
-    # model.addConstr(M >= m)
 
-    model.setObjective(sum([evaluation_function(course, rankings, edge_vars) for course in courses]), GRB.MAXIMIZE)
+    model.setObjective(sum([evaluation_function(course, rankings, edge_vars) for course in courses]) - (M - m), GRB.MAXIMIZE)
 
     return model, edge_vars
+
+courses_df = pd.read_csv('course test - courses.csv')
+applicants_df = pd.read_csv('course test - applicants.csv')
+rankings_df = pd.read_csv('course test - ranking.csv')
+
+courses, tas, rankings, edges = format_dfs(courses_df, applicants_df, rankings_df)
 
 model, edge_vars = build_model(courses, rankings, edges)
 model.optimize()
 
-# for v in model.getVars():
-#     print(v.varName, v.x)
-
-# print('Optimal objective function value', model.objVal)
+print_output(model, edge_vars, tas)
